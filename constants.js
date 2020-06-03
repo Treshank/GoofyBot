37710dcbc5a834c325aa052a93d1c6eed10928e8
Treshank <treshank.prasad@gmail.com>
2020-06-03 09:42:25 +0530

    Fixed bot TOKEN is public.

--- constants.js
+++ constants.js
@@ -5,6 +5,7 @@ function define(name, value) {
     });
 }
 
+//The TOKEN must be defined in a .env file in the root directory in a string 'TOKEN'
 define('TOKEN', process.env.TOKEN)
 define('UPREFIX', '!')
 define('APREFIX', '&')
